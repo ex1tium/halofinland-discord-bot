94013d7e6bfa490c382d4ab48b05241a36419908
ex1tium <mangeria.code@outlook.com>
2022-02-01 01:47:47 +0200

    added uncaughtExceptionHandler. TODO: disable if bad idea

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,32 @@ import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './exception-filters/globalExceptions';
 // import { HttpExceptionFilter } from './globalExceptions';
+// import process, { kill } from 'process';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const { httpAdapter } = app.get(HttpAdapterHost);
 
   app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
+
   await app.listen(3000);
+
 }
+
 bootstrap();
+
+process.on('uncaughtException', (error) => {
+
+  // POSSIBLY BAD IDEA. PREVENT APP FROM CRASHING IF THIS LISTENER IS ON.
+  console.error(`possible fatal error: ${error}`);
+
+
+  // TRIED TO MANUALLY CRASH APP AFTER this listener has triggered. Doesn't work as hoped?
+  // process.kill(process.pid, 'SIGKILL')
+
+  // process.kill(process.pid, 'SIGTERM')
+  // process.exit(process.exitCode)
+
+  // process.abort()
+});
